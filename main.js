1d9a1023c228becb56a6afdeff496a6be860b5af
MATOVU CALEB <58883113+CALEB789@users.noreply.github.com>
2022-08-12 16:06:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const path = require('path')
 let initial_path = path.join(__dirname)
 const port = process.env.PORT || 4000
 const app = express()
-
 app.use(express.static(initial_path))
 app.get('/',(req,res)=>{
   res.sendFile(path.join(initial_path,"index.html"))
